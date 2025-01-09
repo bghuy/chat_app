a1ed78ec92db9bab06f0ca8dbf6e10c77eba137f
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-09 22:29:24 +0700

    first commit

---
+++ src/services/auth.ts
@@ -0,0 +1,13 @@
+import axios from "@/setup/axios";
+export const checkAuth = async () => {
+    try {
+        const response = await axios.get("/auth/profile");
+        return response.data;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log("User not found!", error.message);
+        } else {
+            console.log("User not found!", "An unknown error occurred");
+        }
+    }
+};
